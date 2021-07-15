f1724277bb0b6c664ef1a8678333dad180fa37fc
kano1201 <kano.nakamura3@gmail.com>
2021-07-15 10:38:28 +0000

    make_top.about

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  get 'about' => 'homes#about'
 end
