f6507274b9aa992b51acfd25badf1979f9534853
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-07-16 05:00:58 +0000

    Preparation

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :items, only: [:index, :show]
 end
