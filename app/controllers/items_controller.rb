75f70ea51ee3ef730ff4ce0d7c3fe531ad37786e
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-07-16 10:27:29 +0000

    showのviewに簡単なコーディング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,13 @@
 class ItemsController < ApplicationController
   def index
-
+    @item = Item.find(params[:id])
+    @items = Item.order(id: :desc).page(params[:page]).reverse_order
+    @genres = Genre.all
   end
 
   def show
-
+    @item = Item.find(params[:id])
+    @genres = Genre.all
   end
 
 end
