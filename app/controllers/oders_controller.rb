aa3d1473028929b06a4485acd0ae534c4a513156
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-07-16 06:00:58 +0000

    Preparation

--- app/controllers/oders_controller.rb
+++ app/controllers/oders_controller.rb
@@ -1,2 +1,20 @@
 class OdersController < ApplicationController
+  def new
+  end
+
+  def confirm
+  end
+
+  def create
+  end
+
+  def complete
+  end
+
+  def index
+  end
+
+  def show
+  end
+
 end
