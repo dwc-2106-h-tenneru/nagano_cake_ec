84c72b5d585cb60f8f2212cb1cc8f0d82979ee63
kano1201 <kano.nakamura3@gmail.com>
2021-07-17 14:00:46 +0000

    make_mypage,edit

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,22 @@
 class CustomersController < ApplicationController
   def show
-    @customer = Customer.all
+    @customer = Customer.find(params[:id])
   end
+  
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to customer_path(@customer.id)  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
 
 end
