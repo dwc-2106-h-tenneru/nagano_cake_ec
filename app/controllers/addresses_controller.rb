c827f1c336e03b180a3829c34aaa63a88fcd868f
kano1201 <kano.nakamura3@gmail.com>
2021-07-18 02:27:15 +0000

    add_address_destroy

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -22,6 +22,9 @@ class AddressesController < ApplicationController
   end
 
   def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path
   end
 
    private
