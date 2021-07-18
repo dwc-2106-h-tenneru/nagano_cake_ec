7f9023912a76fd5a074b692c8adbd4557f4f9d98
ishr-sk <kmrttnr1208@gmail.com>
2021-07-18 04:08:48 +0000

    内容変更なし

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,10 +1,6 @@
 class Genre < ApplicationRecord
   has_many :items
-<<<<<<< HEAD
-  
+
   validates :name, presence: true
-  
-=======
-validates :name, presence: true
->>>>>>> origin/develop
+
 end
