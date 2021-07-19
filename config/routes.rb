51838bf1ce98c8041903933217c4f73d70d67c97
EC2 Default User <ec2-user@ip-172-31-83-123.ec2.internal>
2021-07-19 06:36:05 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :admins
   devise_for :customers
+ 
   
   namespace :admin do
     root to: "homes#top"
@@ -9,4 +9,42 @@ Rails.application.routes.draw do
   end
   
   
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    devise_for :admins
+    #root to: 'admins/sessions#new'
+    get '/admins/sign_in'=> 'admins/sessions#new'
+    post '/admins/sign_in' => 'admins/sessions#create'
+    #delete '/admins/sign_out' => 'admins/sessions#destroy'
 end
