62301314a29eb1ecd86edb1d6788ddfa79fc9a61
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-07-22 03:23:35 +0000

    マージさせて

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,13 +3,9 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-<<<<<<< HEAD
    has_many :addresses, dependent: :destroy
-end
-=======
 
   has_many :addresses, dependent: :destroy
   has_many :cart_items
   has_many :orders
 end
->>>>>>> origin/develop
