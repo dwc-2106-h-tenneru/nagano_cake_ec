8e6527d8b2f6cf907c65cbfc26e3e01d387fbdef
MasayaNakamoto <daigoroh67@yahoo.co.jp>
2021-07-26 07:35:05 +0000

    aaa

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def new
     @customer = Customer.find(current_customer.id)
     @order = Order.new
@@ -44,7 +44,7 @@ class OrdersController < ApplicationController
         item: cart_item.item,
         order: @order,
         amount: cart_item.amount,
-        price: cart_item.item.price
+        price: cart_item.item.add_tax_price
       )
       current_customer.cart_items.destroy_all
     end
@@ -64,6 +64,7 @@ class OrdersController < ApplicationController
   end
 
   def show
+    @orders = current_customer.orders.page(params[:page]).reverse_order.order("id DESC")
     @order = Order.find(params[:id])
     @customer = Customer.find(current_customer.id)
     @order.shipping_cost = 800
