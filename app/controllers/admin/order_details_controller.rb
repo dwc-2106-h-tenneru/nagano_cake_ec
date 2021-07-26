cd858df2f4f3c6fdef5648cb61d7f4455afd055d
ishr-sk <kmrttnr1208@gmail.com>
2021-07-26 11:48:42 +0000

    [修正]ステータス管理の連動実装完了

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -4,13 +4,22 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
     @order = @order_detail.order
+    @order_amount = @order.order_details.count
     if @order_detail.update(order_detail_params)
       if @order_detail.making_status == "製作中"
         @order.status = 2
         @order.update(status: @order.status)
-      elsif @order_detail.making_status == "製作完了" #これだと１つ変えるとステータス更新されてしまう
-        @order.status = 3
-        @order.update(status: @order.status)
+      end
+      @count = 0
+      @order.order_details.each do |order_detail|
+        if  order_detail.making_status_before_type_cast == 3
+          @count += 1
+        end
+      end
+      if @count == @order_amount
+        # @order_detail.making_status == "製作完了" #これだと１つ変えるとステータス更新されてしまう
+        # @order.status = 3
+        @order.update(status: 3)
       end
         redirect_to admin_order_path(@order_detail.order)
         flash[:success] = "製作ステータスを更新しました"
@@ -21,10 +30,6 @@ class Admin::OrderDetailsController < ApplicationController
 
   def order_detail_params
     params.require(:order_detail).permit(:making_status)
-  end
-
-  def order_params
-    params.require(:order).permit(:status, :price, :shipping_cost, :customer_id)
   end
 
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -10,11 +10,14 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    @order_detail = @order.order_details.where(making_status: 1)
+    # @order_detail = @order.order_details.where(making_status: 1)
       if @order.update(order_params)
         if @order.status == "入金確認"
-          @order_detail.making_status = 1
-          @order_detail.update(making_status: @order_detail.making_status)
+          @order.order_details.each do |order_detail|
+            # order_detail.making_status = 1
+            order_detail.update(making_status: 1)
+          end
+          # @order_detail.update(making_status: @order_detail.making_status)
           redirect_to admin_order_path(@order)
           flash[:success] = "注文ステータスを更新しました"
         end
