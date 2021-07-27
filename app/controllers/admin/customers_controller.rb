2fa85804165d1603d53324e28d2d651e77f0a649
kano1201 <kano.nakamura3@gmail.com>
2021-07-27 02:52:53 +0000

    admin_customer

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -26,6 +26,6 @@ class Admin::CustomersController < ApplicationController
 
 	private
 	def customer_params
-	  params.require(:customer).permit(:first_name,:last_name,:kana_first_name,:kana_last_name,:postal_code,:phone_number,:email,:is_deleted)
+	  params.require(:customer).permit(:first_name,:last_name,:first_name_kana,:last_name_kana,:post_code,:address,:phone_number,:email,:is_deleted)
 	end
 end
