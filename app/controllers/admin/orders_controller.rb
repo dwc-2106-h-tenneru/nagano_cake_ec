01043929ac2ef431b624fcc73a753731baad0602
EC2 Default User <ec2-user@ip-172-31-83-123.ec2.internal>
2021-07-27 04:52:07 +0000

    コンフリ修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -10,17 +10,6 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-<<<<<<< HEAD
-    @order_detail = @order.order_details
-    if @order.update(order_params)
-      # flash[:success] = "注文ステータスを更新しました"
-      if @order.status == "入金確認"
-        @order_detail.making_status = 1
-        @order_detail.update(order_detail_params)
-      end
-        redirect_to admin_order_path(@order)
-        flash[:success] = "注文ステータスを更新しました"
-=======
     if @order.update(order_params)
       if @order.status == "入金確認"
         @order.order_details.each do |order_detail|
@@ -32,7 +21,6 @@ class Admin::OrdersController < ApplicationController
         redirect_to admin_order_path(@order)
         flash[:success] = "注文ステータスを更新しました"
       end
->>>>>>> origin/develop
     end
   end
 
@@ -47,3 +35,4 @@ class Admin::OrdersController < ApplicationController
   end
 
 end
+
